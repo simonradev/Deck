22b21e9e2c9f24679a61e8059a618d14608e8d1e
simonradev <simonradev@gmail.com>
2017-03-21 10:14:37 +0200

    The code ready to go. Added description in the README.md file.

---
+++ Deck.cs
@@ -0,0 +1,168 @@
+﻿namespace Deck
+{
+    using System;
+    
+    /// <summary>
+    /// This is a linear, generic data type that works like a STACK but has two sides /Right and Left/.
+    /// You can choose into which side you want to insert an element.
+    /// You can retrieve the elements only from the side you inserted it into.
+    /// </summary>
+    /// <typeparam name="TType"></typeparam>
+    class Deck<TType>
+    {
+        private TType[] leftElements;
+        private TType[] rightElements;
+        private int leftCount;
+        private int rightCount;
+        private int leftElementsSize = 6;
+        private int rightElementsSize = 6;
+        private bool rightIsForResizing;
+
+        public Deck()
+        {
+            leftElements = new TType[leftElementsSize];
+            rightElements = new TType[rightElementsSize];
+        }
+
+        public int Count
+        {
+            get { return (this.leftCount + this.rightCount); }
+        }
+
+        public int RightCount
+        {
+            get { return this.rightCount; }
+        }
+
+        public int LeftCount
+        {
+            get { return this.leftCount; }
+        }
+
+        /// <summary>
+        /// Insert the element on the right of the deck.
+        /// </summary>
+        /// <param name="rightElement"></param>
+        public void PushRight(TType rightElement)
+        {
+            if (rightCount == rightElements.Length)
+            {
+                rightIsForResizing = true;
+                DoubleUpTheSize();
+            }
+
+            rightElements[rightCount] = rightElement;
+
+            rightCount++;
+        }
+
+        /// <summary>
+        /// Insert the element on the left of the deck.
+        /// </summary>
+        /// <param name="leftElement"></param>
+        public void PushLeft(TType leftElement)
+        {
+            if (leftCount == leftElements.Length)
+            {
+                rightIsForResizing = false;
+                DoubleUpTheSize();
+            }
+
+            leftElements[leftCount] = leftElement;
+
+            leftCount++;
+        }
+
+        /// <summary>
+        /// Returns and removes the last element from the right side of the deck.
+        /// </summary>
+        /// <returns></returns>
+        public TType PopRight()
+        {
+            ValidateTheOperation(rightCount, "The right deck is empty. No item to pop. Try inserting something first.");
+
+            int indexToRemoveFromRight = rightCount - 1;
+
+            TType elementToReturn = rightElements[indexToRemoveFromRight];
+            rightElements[indexToRemoveFromRight] = default(TType);
+
+            rightCount--;
+
+            return elementToReturn;
+        }
+
+        /// <summary>
+        /// Returns and removes the last element from the left side of the deck.
+        /// </summary>
+        /// <returns></returns>
+        public TType PopLeft()
+        {
+            ValidateTheOperation(leftCount, "The left deck is empty. No item to pop. Try inserting something first.");
+
+            int indexToRemoveFromLeft = leftCount - 1;
+
+            TType elementToReturn = leftElements[indexToRemoveFromLeft];
+            leftElements[indexToRemoveFromLeft] = default(TType);
+
+            leftCount--;
+
+            return elementToReturn;
+        }
+
+        /// <summary>
+        /// Only returns without removing the last element from the right side of the deck.
+        /// </summary>
+        /// <returns></returns>
+        public TType PeekRight()
+        {
+            ValidateTheOperation(rightCount, "The right deck is empty. No item to peek. Try inserting something first.");
+
+            int itemToPeek = rightCount - 1;
+
+            return rightElements[itemToPeek];
+        }
+
+        /// <summary>
+        /// Only returns without removing the last element from the left side of the deck.
+        /// </summary>
+        /// <returns></returns>
+        public TType PeekLeft()
+        {
+            ValidateTheOperation(leftCount, "The left deck is empty. No item to peek. Try inserting something first.");
+
+            int itemToPeek = leftCount - 1;
+
+            return leftElements[itemToPeek];
+        }
+
+        private void DoubleUpTheSize()
+        {
+            if (rightIsForResizing)
+            {
+                rightElementsSize *= 2;
+                TType[] rightArrHolder = new TType[rightElements.Length];
+                Array.Copy(rightElements, rightArrHolder, rightElements.Length);
+                rightElements = new TType[rightElementsSize];
+                Array.Copy(rightArrHolder, rightElements, rightArrHolder.Length);
+                rightArrHolder = default(TType[]);
+            }
+            else
+            {
+                leftElementsSize *= 2;
+                TType[] leftArrHolder = new TType[leftElements.Length];
+                Array.Copy(leftElements, leftArrHolder, leftArrHolder.Length);
+                leftElements = new TType[leftElementsSize];
+                Array.Copy(leftArrHolder, leftElements, leftArrHolder.Length);
+                leftArrHolder = default(TType[]);
+            }
+        }
+
+        private void ValidateTheOperation(int count, string message)
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException(message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Deck
+
+This is my first implementation of the deck. It is a linear data structure which is also 
+generic and static. It works just like a Stack<> (LIFO) but you it has two sides which hold elements 
+a left one and a right one. Separately they work just like a Stack<> but in the deck
+they are connected. This means you can insert from the left and from the right and when 
+you want to retrieve an element you choose from which side and you get the last one that
+was pushed/inserted. Static means that the left and the right elements are arrays which have default size of 6 
+and double up their sizes when you fill them up.
+
+These are the implemented methods:
+- PeekLeft();
+- PeekRight();
+- PopLeft();
+- PopRight();
+- PushLeft();
+- PushRight();
+
+There are the implemented properties:
+- Count;
+- LeftCount;
+- RightCount;
